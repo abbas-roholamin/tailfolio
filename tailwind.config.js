98ca8f983211c1f74c511c2aab9b263b5a8d465d
Abbas roholamin <92622822+abbas-roholamin@users.noreply.github.com>
2022-04-24 02:59:22 +0430

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @format */
+
+module.exports = {
+  darkMode: "class",
+  content: ["./*.html", "./assets/**/*.js"],
+  theme: {},
+  variants: {
+    extend: {},
+  },
+  plugins: [require("plugin")],
+};
